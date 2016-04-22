1ed4a58de23972240e6e755c8b108222d897f350
Jacques Heunis <jaaksemail@gmail.com>
2016-04-23 00:19:42 +0200

    Disable incremental linking

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 @echo off
 set CompileFiles= ..\src\main.cpp ..\src\graphics.cpp ..\src\graphicsutil.cpp ..\src\vecmath.cpp ..\src\audio.cpp ..\src\escapi.cpp ..\imgui\gl3w.cpp ..\imgui\imgui.cpp ..\imgui\imgui_draw.cpp ..\imgui\imgui_impl_sdl_gl3.cpp
-set CompileFlags= -nologo -Zi -Gm- -W4 -D_CRT_SECURE_NO_WARNINGS -MT
+set CompileFlags= -nologo -Zi -Gm- -W4 -D_CRT_SECURE_NO_WARNINGS -MD
 set IncludeDirs= -I..\src -I..\include -I..\dge
 
 set EnetLibs=..\lib\enet64.lib ws2_32.lib winmm.lib
@@ -9,7 +9,7 @@ set DaalaLibs=..\lib\LibDaalaBase.lib ..\lib\libdaaladec.lib ..\lib\libdaalaenc.
 set LinkLibs= OpenGL32.lib SDL2.lib ..\lib\escapi.lib  %EnetLibs% %OpusLibs% %DaalaLibs% ..\lib\libsoundio.dll.a
 
 pushd build
-cl %CompileFlags% %CompileFiles% %IncludeDirs% -link %LinkLibs% -OUT:main.exe
+cl %CompileFlags% %CompileFiles% %IncludeDirs% -link %LinkLibs% -INCREMENTAL:NO -OUT:main.exe
 popd
 
 
