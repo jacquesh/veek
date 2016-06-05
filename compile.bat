fddc54401de00449435c35de9d1517b0da5a630d
Jacques Heunis <jaaksemail@gmail.com>
2016-06-05 17:06:44 +0200

    Remove some unnecessary compilation work

--- compile.bat
+++ compile.bat
@@ -4,6 +4,7 @@ ctime -begin veek_time.ctm
 
 set CompileFiles= ..\src\main.cpp ..\src\graphics.cpp ..\src\graphicsutil.cpp ..\src\vecmath.cpp ..\src\audio.cpp ..\src\video.cpp ..\src\ringbuffer.cpp ..\src\win32_platform.cpp ..\src\logging.cpp ..\src\escapi.cpp ..\imgui\gl3w.cpp ..\imgui\imgui.cpp ..\imgui\imgui_draw.cpp ..\imgui\imgui_impl_glfw_gl3.cpp
 set CompileFlags= -nologo -Zi -Gm- -W4 -D_CRT_SECURE_NO_WARNINGS -DNOMINMAX -MDd
+set CompileFlags= -nologo -Zi -Gm- -W4 -D_CRT_SECURE_NO_WARNINGS -DNOMINMAX -MDd -EHsc-
 set IncludeDirs= -I..\include
 
 set GLFWLibs=glfw3.lib gdi32.lib shell32.lib
@@ -11,8 +12,7 @@ set EnetLibs=enet.lib ws2_32.lib winmm.lib
 set OpusLibs=opus.lib celt.lib silk_common.lib silk_fixed.lib silk_float.lib
 set TheoraLibs=libtheora_static.lib libogg_static.lib
 set SoundIOLibs=soundio.lib ole32.lib
-REM set LinkLibs= OpenGL32.lib ..\lib\escapi.lib %GLFWLibs% %EnetLibs% %OpusLibs% %TheoraLibs% ..\lib\libsoundio.dll.a
-set LinkLibs= OpenGL32.lib escapi.lib %GLFWLibs% %EnetLibs% %OpusLibs% %TheoraLibs% %SoundIOLibs%
+set LinkLibs= OpenGL32.lib escapi.lib imgui.lib %GLFWLibs% %EnetLibs% %OpusLibs% %TheoraLibs% %SoundIOLibs%
 
 pushd build
 cl %CompileFlags% %CompileFiles% %IncludeDirs% -DSOUNDIO_STATIC_LIBRARY -link -LIBPATH:..\lib %LinkLibs% -INCREMENTAL:NO -OUT:main.exe
--- src/win32_platform.cpp
+++ src/win32_platform.cpp
@@ -1,6 +1,8 @@
 #include "platform.h"
 
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <stdlib.h>
 
 #include "common.h"
 
