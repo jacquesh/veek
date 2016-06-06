4fdc562c7f07276b7505c5c84c2735c236a19ea9
Jacques Heunis <jaaksemail@gmail.com>
2016-06-06 23:45:11 +0200

    Fix some minor errors in the compile scripts

--- compile-server.bat
+++ compile-server.bat
@@ -1,6 +1,6 @@
 @echo off
 set CompileFiles= ..\src\server.cpp
-set CompileFlags= -nologo -Zi -Gm- -W4 -D_CRT_SECURE_NO_WARNINGS -MDd
+set CompileFlags= -nologo -Zi -Gm- -W4 -D_CRT_SECURE_NO_WARNINGS -MTd
 set IncludeDirs= -I..\include
 
 set LinkLibs=enet.lib ws2_32.lib winmm.lib
--- compile.bat
+++ compile.bat
@@ -2,7 +2,7 @@
 
 ctime -begin veek_time.ctm
 
-set CompileFiles= ..\src\main.cpp ..\src\graphics.cpp ..\src\graphicsutil.cpp ..\src\vecmath.cpp ..\srcvideo.cpp ..\src\audio.cpp  ..\src\ringbuffer.cpp ..\src\win32_platform.cpp ..\src\logging.cpp ..\src\escapi.cpp
+set CompileFiles= ..\src\main.cpp ..\src\graphics.cpp ..\src\graphicsutil.cpp ..\src\vecmath.cpp ..\src\video.cpp ..\src\audio.cpp  ..\src\ringbuffer.cpp ..\src\win32_platform.cpp ..\src\logging.cpp ..\src\escapi.cpp
 set CompileFlags= -nologo -Zi -Gm- -W4 -D_CRT_SECURE_NO_WARNINGS -DNOMINMAX -MTd -EHsc-
 set IncludeDirs= -I..\include
 
@@ -10,7 +10,7 @@ set GLFWLibs=glfw3.lib gdi32.lib shell32.lib
 set EnetLibs=enet.lib ws2_32.lib winmm.lib
 set OpusLibs=opus.lib celt.lib silk_common.lib silk_fixed.lib silk_float.lib
 set TheoraLibs=libtheora_static.lib libogg_static.lib
-set SoundIOLibs=soundio.lib ole32.lib
+set SoundIOLibs=soundio_static.lib ole32.lib
 set LinkLibs= OpenGL32.lib escapi.lib imgui.lib %GLFWLibs% %EnetLibs% %OpusLibs% %TheoraLibs% %SoundIOLibs%
 
 pushd build
