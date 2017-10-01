9691bd8316e7f1d28e0a96930827f0f3cbc86bab
Jacques Heunis <jaaksemail@gmail.com>
2017-10-01 11:22:41 +0200

    Update windows compile scripts to work without library includes in include/

--- compile-server.bat
+++ compile-server.bat
@@ -1,12 +1,12 @@
 @echo off
 set CompileFiles= ..\src\server.cpp ..\src\user.cpp ..\src\network.cpp ..\src\platform.cpp ..\src\logging.cpp ..\src\happyhttp.cpp
 set CompileFlags= -nologo -Zi -Gm- -W4 -D_CRT_SECURE_NO_WARNINGS -DNOMINMAX -MTd -EHsc
-set IncludeDirs= -I..\include
+set IncludeDirs= -I..\thirdparty\include
 
 set LinkLibs=enet.lib ws2_32.lib winmm.lib user32.lib
 
 pushd build
-cl %CompileFlags% %CompileFiles% %IncludeDirs% -link -LIBPATH:..\lib %LinkLibs% -INCREMENTAL:NO -OUT:server.exe
+cl %CompileFlags% %CompileFiles% %IncludeDirs% -link -LIBPATH:..\thirdparty\lib\win64 %LinkLibs% -INCREMENTAL:NO -OUT:server.exe
 popd
 
 
--- compile.bat
+++ compile.bat
@@ -8,18 +8,19 @@ FOR /f %%H IN ('git log -n 1 --oneline') DO set VersionHash=%%H
 set CompileFiles= ..\src\main.cpp ..\src\render.cpp ..\src\audio.cpp ..\src\audio_resample.cpp ..\src\ringbuffer.cpp ..\src\platform.cpp ..\src\logging.cpp ..\src\user.cpp ..\src\user_client.cpp ..\src\network.cpp ..\src\network_client.cpp ..\src\video.cpp ..\src\videoinput.cpp
 REM  ..\src\happyhttp.cpp
 set CompileFlags= -nologo -Zi -Gm- -W4 -wd4100 -D_CRT_SECURE_NO_WARNINGS -Od -DNOMINMAX -MTd -EHsc- -DBUILD_VERSION=\"%VersionHash%_%BuildDate%_%BuildTime%\" -DSOUNDIO_STATIC_LIBRARY
-set IncludeDirs= -I..\include
+set IncludeDirs= -I..\include -I..\thirdparty\include
 
-set GLFWLibs=glfw3.lib gdi32.lib shell32.lib
+set GLFWLibs=glfw3.lib gdi32.lib shell32.lib OpenGL32.lib imgui.lib
 set EnetLibs=enet.lib ws2_32.lib winmm.lib
 set OpusLibs=opus.lib celt.lib silk_common.lib silk_fixed.lib silk_float.lib
 set TheoraLibs=libtheora_static.lib libogg_static.lib
 set SoundIOLibs=libsoundio_static.lib ole32.lib
 set videoInputLibs=OleAut32.lib Strmiids.lib
-set LinkLibs= OpenGL32.lib imgui.lib %GLFWLibs% %EnetLibs% %OpusLibs% %TheoraLibs% %SoundIOLibs% %videoInputLibs%
+set LinkLibs=%GLFWLibs% %EnetLibs% %OpusLibs% %TheoraLibs% %SoundIOLibs% %videoInputLibs%
+set LinkFlags=-LIBPATH:..\thirdparty\lib\win64 %LinkLibs% -INCREMENTAL:NO -OUT:main.exe
 
 pushd build
-cl %CompileFlags% %CompileFiles% %IncludeDirs% -link -LIBPATH:..\lib %LinkLibs% -INCREMENTAL:NO -OUT:main.exe
+cl %CompileFlags% %CompileFiles% %IncludeDirs% -link %LinkFlags%
 popd
 
 ctime -end veek_time.ctm %ERRORLEVEL%
